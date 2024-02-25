b83c80fdec03a0ae86aeed3e32e8f149dbacb762
mengdcn <154581057+mengdcn@users.noreply.github.com>
2024-02-25 23:46:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("test main 1")
 
+	//分支更新
         fmt.Println("fork result")
-
+	
+	//分支更新
 	fmt.Println("fork result 2")
-
+	
+	//分支更新
 	fmt.Println("fork result 3")
+	
+	//源更新
+	fmt.Println("test main 1")
+
 
 }
